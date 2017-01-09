4d10a17a0bf34b76a3f7f1fd607c502bd8e12759
Monika Zarako-Dobrowner <Monika@Monikas-iMac.local>
2017-01-09 09:03:51 -0800

    broken toggle sidebar fx, created sidebar components

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import GridContainer from './grid-container';
-import Sidebar from './sidebar';
+import SidebarContainer from './sidebar';
 import Explanation from './explanation';
 
 export default () => {
   return (
     <div className="app">
-      <div className="button" id="show-hide">hide sidebar</div>
-      <Sidebar />
+      <SidebarContainer />
       <GridContainer />
       <Explanation />
     </div>
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,14 +6,17 @@ import changeSpeed from '../actions/changeSpeed';
 import pause from '../actions/pause';
 import changeColors from '../actions/changeColors';
 import colorChoices from '../colors';
+import Swatch from './sidebar-components/swatch';
+import InitialState from './sidebar-components/initial-states';
 
-class Sidebar extends Component {
+class SidebarContainer extends Component {
   constructor () {
     super();
-    this.state = { showExplanation: false };
+    this.state = { showExplanation: false, showSidebar: true };
     this.toggleExplanation = this.toggleExplanation.bind(this);
     this.changeSpeed = this.changeSpeed.bind(this);
     this.changeColors = this.changeColors.bind(this);
+    this.toggleSidebar = this.toggleSidebar.bind(this);
   }
   toggleExplanation() {
     this.props.pause();
@@ -27,6 +30,24 @@ class Sidebar extends Component {
     let htmlTag = document.getElementsByTagName("HTML")[0];
     htmlTag.style.backgroundColor = newColors[0];
   }
+  toggleSidebar() {
+    console.log('entered toggle');
+    const colors = this.props.colors;
+    let sidebarStylingWidth = {
+        backgroundColor: colors[0],
+        color: colors[3],
+        height: "100%"
+      }
+    if (this.state.showSidebar) {
+      this.setState({showSidebar: !this.state.showSidebar});
+      sidebarStylingWidth.width = "0";
+    }
+    else {
+      this.setState({showSidebar: true});
+      sidebarStylingWidth.width = "200px";
+    }
+    return sidebarStylingWidth;
+  }
   render() {
     const colors = this.props.colors;
     const sidebarStyling = {
@@ -36,61 +57,32 @@ class Sidebar extends Component {
     let htmlTag = document.getElementsByTagName("HTML")[0];
     htmlTag.style.backgroundColor = colors[0];
     return (
-      <div className="sidebar" style={sidebarStyling}>
-        <div className="button" id="show-hide">hide sidebar</div>
-        <h1 className="title"> cellular <br /> automaton </h1>
-        <div className="what-is-this button" onClick={this.toggleExplanation}>
-          <h3>what is this?</h3>
+      <div className="sidebar-container">
+        <div className="button" id="show-hide" style={sidebarStyling} onClick={ this.toggleSidebar }>hide</div>
+        <div className="sidebar" style={this.toggleSideBar}>
+          <h1 className="title"> cellular <br /> automaton </h1>
+          <div className="what-is-this button" onClick={this.toggleExplanation}>
+            <h3>what is this?</h3>
+          </div>
+          <h2 className="user-controls">controls</h2>
+            <h3 className="options color">color</h3>
+              <Swatch colors={colorChoices.green} onClick={()=>{props.changeColors(colorChoices.green)}} />
+              <Swatch colors={colorChoices.purple} onClick={()=>{props.changeColors(colorChoices.purple)}} />
+              <Swatch colors={colorChoices.southwest} onClick={()=>{props.changeColors(colorChoices.southwest)}} />
+            <h3 className="options speed">speed</h3>
+              <div className="option button" onClick={ () => this.changeSpeed(1500) }> > </div>
+              <div className="option button" onClick={ () => this.changeSpeed(1000) }> >> </div>
+              <div className="option button" onClick={ () => this.changeSpeed(600) }> >>> </div>
+            <h3 className="options initial-state">initial state</h3>
+              <InitialState initial={'little triangle'} />
+              <InitialState initial={'big triangle'} />
+              <InitialState initial={'hexagon'} />
+              <InitialState initial={'border'} />
+              <InitialState initial={'face'} />
+              <InitialState initial={'triforce'} />
+              <InitialState initial={'other'} />
         </div>
-        <h2 className="user-controls">controls</h2>
-          <h3 className="options color">color</h3>
-            <div className="option button green"
-              onClick={()=>{this.changeColors(colorChoices.green)}}>
-              <div>
-                <div className="color-sample" id="green0"></div>
-                <div className="color-sample" id="green1"></div>
-              </div>
-              <div>
-                <div className="color-sample" id="green2"></div>
-                <div className="color-sample" id="green3"></div>
-              </div>
-            </div>
-            <div className="option button purple"
-              onClick={()=>{this.changeColors(colorChoices.purple)}}>
-               <div>
-                <div className="color-sample" id="purple0"></div>
-                <div className="color-sample" id="purple1"></div>
-              </div>
-              <div>
-                <div className="color-sample" id="purple2"></div>
-                <div className="color-sample" id="purple3"></div>
-              </div>
-
-            </div>
-            <div className="option button southwest"
-              onClick={()=>{this.changeColors(colorChoices.southwest)}}>
-              <div>
-                <div className="color-sample" id="southwest0"></div>
-                <div className="color-sample" id="southwest1"></div>
-              </div>
-              <div>
-                <div className="color-sample" id="southwest2"></div>
-                <div className="color-sample" id="southwest3"></div>
-              </div>
-            </div>
-
-          <h3 className="options speed">speed</h3>
-            <div className="option button" onClick={ () => this.changeSpeed(1500) }> > </div>
-            <div className="option button" onClick={ () => this.changeSpeed(1000) }> >> </div>
-            <div className="option button" onClick={ () => this.changeSpeed(600) }> >>> </div>
-          <h3 className="options initial-state">initial state</h3>
-            <div className="option button"><p>little triangle</p></div>
-            <div className="option button"><p>big triangle</p></div>
-            <div className="option button"><p>hexagon</p></div>
-            <div className="option button"><p>border</p></div>
-            <div className="option button"><p>face</p></div>
-            <div className="option button"><p>triforce</p></div>
-      </div>
+      </div>  
     )
   }
 }
@@ -113,4 +105,4 @@ function mapDispatchToProps(dispatch) {
   return bindActionCreators(actions, dispatch);
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarContainer);
