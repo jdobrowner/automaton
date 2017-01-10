4e8d6490fd8cc9ff91630072b8aedcf7d238eddf
jdobrowner <j@jasondobrowner.com>
2017-01-10 14:02:08 -0800

    fixed merge problem

--- src/components/explanation.js
+++ src/components/explanation.js
@@ -23,31 +23,10 @@ class Explanation extends Component {
           <span>The simplest model of a cellular automaton is a 1D collection</span> of square cells in a row, each of which has a state of either 0 (white) or 1 (black). In this <a target="blank" href="http://mathworld.wolfram.com/ElementaryCellularAutomaton.html">model</a>, each cell has two neighbors– one to its left and one to its right. The cells' states change according to a set of rules. For example, if the current cell state is 0, its left neighbor is 0, and its right neighbor's is 1, change the current cell state to 1. For this most simple model, there exist 256 possible rulesets/combinations, creating a variety of visual patterns. <br /><br /><br />
 
           <span>Our cellular automaton features a triangular grid</span> in which each cell can have one of four states– expressed visually as four unique colors. Every cell has three adjacent neighbors which share a border. Because the grid is finite, cells on the edge appear to lack one neighbor. These edge cells are programmed to wrap around the x- and y- axis to find their neighbors on the other side, creating a <a target="blank" href="https://www.google.com/search?q=torus&espv=2&biw=2024&bih=982&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjWm_3awbjRAhXorFQKHfvXB6QQ_AUIBigB">torus-shaped</a> system of cells. After calulating its next state based on its current state and the state of its three neighbors, each cell updates its color accordingly. <br /><br /><br />
-          
+
           If you'd like to read more about cellular automata, here are some resources: <br />
             <a target="blank" href="https://en.wikipedia.org/wiki/Cellular_automaton">Wikipedia: Cellular Automaton</a> <br />
             <a target="blank" href="http://natureofcode.com/book/chapter-7-cellular-automata/">Daniel Shiffman: The Nature of Code</a> <br /><br /><br />
-
-          We hope you enjoy the patterns of stability and randomness that create intricate visual displays. Be sure to play around with initial states, rulesets, colors, and adding a little randomness. Have fun! <br /><br />
-          </h3>
-
-          <div className="explanation-images">
-            <div>
-              <img src="./Torus.png" />
-            </div>
-            <div>
-              <div id="cell-1">0</div>
-              <div id="cell-2">1</div>
-              <div id="cell-3">1</div>
-              <div id="cell-3-new">0</div>
-            </div>
-          </div>
-
-          <h3>If you'd like to read more about cellular automata, here are some resources: <br />
-            <a href="https://en.wikipedia.org/wiki/Cellular_automaton" target="_blank">Wikipedia: Cellular Automaton</a> <br />
-            <a href="http://mathworld.wolfram.com/ElementaryCellularAutomaton.html" target="_blank">Stephen Wolfram: Elementary Cellular Automataton</a> <br />
-            <a href="http://natureofcode.com/book/chapter-7-cellular-automata/" target="_blank">Daniel Shiffman: The Nature of Code</a>
-          </h3>
             We hope you enjoy the intricate visual patterns of stability and randomness. Be sure to play around with initial states, rulesets, colors, and adding a little randomness. Have fun!
           </p>
       </div>
