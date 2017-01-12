496f96fd15ab7909eeee08c940cae65bce09adbc
Monika Zarako-Dobrowner <Monika@Monikas-iMac.local>
2017-01-12 07:00:22 -0800

    borders added to buttons

--- src/components/sidebar-components/ruleset.js
+++ src/components/sidebar-components/ruleset.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export default (props) => {
+	const colors = props.colors;
+	const styling = {backgroundColor: colors[0], border: `1px solid ${colors[3]}`};
 	return (
-  	<div className="ruleset option button" onClick={ () => {props.onRulesetClick(props.title)} }>
+  	<div className="ruleset option button" onClick={ () => {props.onRulesetClick(props.title)}} style={styling}>
       <p> {props.title} </p>
     </div>
 		);
