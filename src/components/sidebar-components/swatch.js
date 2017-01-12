c8396e1716c1adc63589580cb434d7ebc9478d70
Monika Zarako-Dobrowner <Monika@Monikas-iMac.local>
2017-01-12 15:43:50 -0800

    readMe added with screenshots, explanation grammar fixed, borders removed

--- src/components/sidebar-components/swatch.js
+++ src/components/sidebar-components/swatch.js
@@ -4,7 +4,7 @@ export default (props) => {
 	const colors = props.colors;
   const currentColors = props.currentColors;
 	return (
-		<div className="option button swatch" onClick={()=>{props.onColorClick(colors)}} style={{border: `1px solid ${currentColors[3]}`}}>
+		<div className="option button swatch" onClick={()=>{props.onColorClick(colors)}} >
           <div>
             <div className="color-sample" style={{backgroundColor: colors[0]}}></div>
             <div className="color-sample" style={{backgroundColor: colors[1]}}></div>
@@ -16,3 +16,6 @@ export default (props) => {
         </div>
 		);
 }
+
+
+// style={{border: `1px solid ${currentColors[1]}`}}
