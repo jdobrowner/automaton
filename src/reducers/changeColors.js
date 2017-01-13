376fed9f5fc01f6f48b8ad0abeb38ada1c4d4ae5
jdobrowner <j@jasondobrowner.com>
2017-01-12 18:18:37 -0800

    changed initial color to always be southwest colors

--- src/reducers/changeColors.js
+++ src/reducers/changeColors.js
@@ -2,16 +2,7 @@ import { COLORS } from '../actions/types';
 import colors from '../colors';
 
 function getInitialColor() {
-  const rand = Math.floor( Math.random() * Object.keys(colors).length );
-  switch (rand) {
-    case 0: return colors.purple;
-    case 1: return colors.green;
-    case 2: return colors.brights;
-    case 3: return colors.red;
-    case 4: return colors.ocean;
-    case 5: return colors.southwest;
-    default: return colors.purple;
-  }
+  return colors.southwest;
 }
 
 export default function cycle(state = getInitialColor(), action) {
