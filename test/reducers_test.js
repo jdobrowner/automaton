533bbef1744b3ce74cfdfb96082c158017e859f3
mzarako <jdobrowner@gmail.com>
2017-03-13 09:22:20 -0700

    added more tests for reducers

--- test/reducers_test.js
+++ test/reducers_test.js
@@ -3,12 +3,18 @@ import { CYCLE, EXPLAIN, PAUSE, SPEED, COLORS, PATTERN, RULESET } from '../src/a
 import changeColors from '../src/reducers/changeColors';
 import changeRuleset from '../src/reducers/changeRuleset';
 import cycle from '../src/reducers/cycle';
-import patternSwitch from '../src/reducers/patternSwitch';
 import pause from '../src/reducers/pause';
 import showExplanation from '../src/reducers/showExplanation';
 import speed from '../src/reducers/speed';
 
+// for the changeColors reducer
 import colors from '../src/colors';
+
+// for the cycle reducer
+import initialStates from '../src/reducers/initialStates/index';
+import automate from '../src/reducers/rulesets/automate';
+import rules from '../src/reducers/rulesets/rules';
+
 
 describe('Colors Reducer', () => {
   const action = { type: COLORS, colors: colors.ocean };
@@ -32,6 +38,43 @@ describe('Ruleset Reducer', () => {
 });
 
 
+describe('Cycle Reducer', () => {
+  const cycleAction = { type: CYCLE, ruleset: 'harmony' };
+  const patternAction = { type: PATTERN, pattern: 'little triangle' };
+  const rulesetAction = { type: RULESET, ruleset: 'harmony' };
+  it('sets the default pattern to be a hexagon', () => {
+    expect(cycle(undefined, {})).to.eql(initialStates.hexagon());
+  });
+  it('cycles with the correct ruleset and pattern', () => {
+    expect(cycle(initialStates.oneTriangle(), cycleAction))
+      .to.eql(automate(initialStates.oneTriangle(), rules.harmony));
+  });
+  it('updates the pattern', () => {
+    expect(cycle(initialStates.hexagon(), patternAction)).to.eql(initialStates.oneTriangle());
+  });
+  it('uses the correct ruleset', () => {
+    expect(cycle(initialStates.oneTriangle(), rulesetAction))
+      .to.eql(automate(initialStates.oneTriangle(), rules.harmony));
+  });
+});
+
+
+describe('Show Explaination Reducer', () => {
+  const action = { type: EXPLAIN };
+
+  it('sets the default show explanation state to false', () => {
+    expect(showExplanation(undefined, {})).to.eql(false);
+  });
+
+  it('switches the explanation state from false to true', () => {
+    expect(showExplanation(false, action)).to.eql(true);
+  });
+
+  it('switches the explanation state from true to false', () => {
+    expect(showExplanation(true, action)).to.eql(false);
+  });
+});
+
 
 describe('Pause Reducer', () => {
   const action = { type: PAUSE };
@@ -46,5 +89,18 @@ describe('Pause Reducer', () => {
 
   it('switches the pause state from true to false', () => {
     expect(pause(true, action)).to.eql(false);
+  });
+});
+
+
+describe('Speed Reducer', () => {
+  const action = { type: SPEED, speed: 600 };
+
+  it('sets the default speed state to 1000', () => {
+    expect(speed(undefined, {})).to.eql(1000);
+  });
+
+  it('changes the speed state', () => {
+    expect(speed(1000, action)).to.eql(600);
   });
 });
